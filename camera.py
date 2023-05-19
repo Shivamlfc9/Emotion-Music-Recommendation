6def5786ff993ceb9cccbf2c86156b15259ae8e2
Shivamlfc9 <44423476+Shivamlfc9@users.noreply.github.com>
2023-05-19 14:40:11 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -75,7 +75,7 @@ class FPS:
 class WebcamVideoStream:
     	
 		def __init__(self, src=0):
-			self.stream = cv2.VideoCapture(0)
+			self.stream = cv2.VideoCapture(1)
 			(self.grabbed, self.frame) = self.stream.read()
 			self.stopped = False
 
