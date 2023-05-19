3784fff24d1f4b2f3efd617baef8b6c734bc9b58
Shivamlfc9 <44423476+Shivamlfc9@users.noreply.github.com>
2023-05-19 15:02:34 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,7 +1,5 @@
 import numpy as np
 import cv2
-vid=cv2.VideoCapture(0, cv2.CAP_DSHOW)
-vid=cv2.VideoCapture(2, cv2.CAP_DSHOW)
 from PIL import Image
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
@@ -77,7 +75,8 @@ class FPS:
 class WebcamVideoStream:
     	
 		def __init__(self, src=0):
-			self.stream = cv2.VideoCapture(src,cv2.CAP_DSHOW)
+			self.stream = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+			self.stream = cv2.VideoCapture(2, cv2.CAP_DSHOW)
 			(self.grabbed, self.frame) = self.stream.read()
 			self.stopped = False
 
